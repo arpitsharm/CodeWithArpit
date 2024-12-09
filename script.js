933b41f8053102e2a02a2cbd0ee1b152f0dfa0e1
CodeWithArpit <150226804+arpitsharm@users.noreply.github.com>
2024-12-09 18:11:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+var tl = gsap.timeline()
 
+tl.from("h1", 
+    {
+        y : -30, 
+        opacity: 0,
+        duration : 1,
+        dealy: 0.5
+    }
+)
+
+tl.from(".part", 
+    {
+        y : -1, 
+        opacity: 0,
+        duration : 2,
+        dealy: 1,
+    }
+)
+
+tl.to(".box1", 
+    {
+        x : 1000, 
+        scale: 0,
+        duration : 1,
+        dealy: 1,
+    }
+)
+
+tl.to(".box2", 
+    {
+        x : 1000, 
+        scale: 0,
+        duration : 1,
+        dealy: 1,
+    }
+)
